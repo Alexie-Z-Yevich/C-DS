14d9acbb667f0c620f5974a677cbee5c1769a6b8
Alexie-Z-Yevich <1213791406@qq.com>
2023-12-27 21:17:48 +0800

    UPDATE

--- test/main.c
+++ test/main.c
@@ -23,18 +23,10 @@ void quicksort(int a[], int l, int r){
     }
 }
 int main() {
-    int a = 1, b = 2;
-    int c = 2;
-    a=b = c+b;
-//    int i = -8567;
-//    char s = 'shs';
-//    printf("%s", s);
-//    printf("%d", i);
-    int a[5] = {1, 3, 4, 2, 5};
-    quicksort(a, 0, 4);
-    for (int i = 0; i < 5; ++i) {
-        printf("%d\n", a[i]);
-    }
+    int x = 10;
+    int y = (2 * 8, x += 5);
+    printf("%d, %d", x, y);
+
 
 }
 
